afa855ff492e8934ea3596c2f4b7951065a56ebe
Narges <narges.montazeri81@gmail.com>
2021-03-30 13:19:24 +0430

    add classes to simple program

--- server.cpp
+++ server.cpp
@@ -1,170 +1,164 @@
 //server
+// client:O,-1    server:x,-2
 #include <iostream>
 #include <boost/asio.hpp>
-#include <thread>
 #include <vector>
-#include <string>
 
 using namespace std;
 using namespace boost::asio;
 using namespace ip;
 
-//functions
-void init_ground(char ground[3][3]);
-void update_ground(char ground[3][3], string s, int c);
-void show_ground(char ground[3][3]);
-void judge(char ground[3][3]);
-
-int main()
+//player class 
+class player
 {
-	//first
-	io_service io;
-	tcp::socket server_sock(io);
-	tcp::acceptor acc(io, tcp::endpoint(tcp::v4(), 1234));
-	acc.accept(server_sock);
-	char ground[3][3];
-	init_ground(ground);
-	show_ground(ground);
+public:
+	player(io_service& io_service, tcp::acceptor& acc);
+	void playgame();
+	void write_move();
+	void read_move();
+private:
+	tcp::socket sock;
+};
 
-	while (1)
-	{
-
-		boost::asio::streambuf buff;
-		read_until(server_sock, buff, "\n");
-		string s = buffer_cast<const char*>(buff.data());
-		update_ground(ground, s, 1);
-		show_ground(ground);
-		judge(ground);
-
-		string msg;
-		getline(cin, msg);
-		msg += "\n";
-		update_ground(ground, msg, 0);
-		show_ground(ground);
-		write(server_sock, boost::asio::buffer(msg));
-		judge(ground);
-	}
-
-
-
-	//second
-
-	//thread t1(sendTo, ref(server_sock));
-	//thread t2(receiveFrom, ref(server_sock));
-
-	//t1.join();
-	//t2.join();
-
-}
-void init_ground(char ground[3][3])
+//ground class
+class ground
 {
-	char c = '1';
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			ground[i][j] = c;
-			c++;
-		}
-	}
-}
-void update_ground(char ground[3][3], string s, int c)
-{
-	int num = atoi(s.c_str());
-	int i;
-	int j;
-	if (num % 3 == 0)
-	{
-		j = 2;
-		i = num / 3 - 1;
-	}
-	else
-	{
-		j = (num % 3) - 1;
-		i = (num / 3);
-	}
-	if (c == 0)
-		ground[i][j] = '#';
-	else
-		ground[i][j] = '*';
+public:
+	ground();
+	void show_ground();
+	void update_ground(int numblock, int who, player* pl);
+	void judge();
+private:
+	int blocks[9];
+	const vector<vector<int>> winblocks = { {0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6} };
+}gr;
 
+ground::ground()
+{
+	for (int i = 0; i < 9; i++)
+		blocks[i] = i + 1;
 }
-void show_ground(char ground[3][3])
+void ground::show_ground()
 {
 	cout << endl;
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 3; j++)
-			cout << ground[i][j] << "  ";
-		cout << endl;
-	}
-}
-void judge(char ground[3][3])
-{
-	for (int i = 0; i < 3; i++)
-	{
-		if (ground[i][0] == ground[i][1] && ground[i][0] == ground[i][2] && ground[i][0] == '#')
-		{
-			cout << "# won!" << endl;
-			exit(0);
-		}
-		if (ground[i][0] == ground[i][1] && ground[i][0] == ground[i][2] && ground[i][0] == '*')
-		{
-			cout << "* won!" << endl;
-			exit(0);
-		}
-	}
-	for (int i = 0; i < 3; i++)
-	{
-		if (ground[0][i] == ground[1][i] && ground[0][i] == ground[2][i] && ground[0][i] == '#')
-		{
-			cout << "# won!" << endl;
-			exit(0);
-		}
-		if (ground[0][i] == ground[1][i] && ground[0][i] == ground[2][i] && ground[0][i] == '*')
-		{
-			cout << "* won!" << endl;
-			exit(0);
-		}
-	}
-	if (ground[0][0] == ground[1][1] && ground[0][0] == ground[2][2] && ground[0][0] == '#')
-	{
-		cout << "# won!" << endl;
-		exit(0);
-	}
-	if (ground[0][0] == ground[1][1] && ground[0][0] == ground[2][2] && ground[0][0] == '*')
-	{
-		cout << "* won!" << endl;
-		exit(0);
-	}
-
-	if (ground[0][2] == ground[1][1] && ground[2][0] == ground[1][1] && ground[1][1] == '#')
-	{
-		cout << "# won!" << endl;
-		exit(0);
-	}
-	if (ground[0][2] == ground[1][1] && ground[2][0] == ground[1][1] && ground[1][1] == '*')
-	{
-		cout << "* won!" << endl;
-		exit(0);
-	}
-	bool flag = TRUE;
+	cout << "     |     |     " << endl;
 	for (int i = 0; i < 3; i++)
 	{
 		for (int j = 0; j < 3; j++)
 		{
-			if (!(ground[i][j] == '*' || ground[i][j] == '#'))
+			if (blocks[3 * i + j] == -1)
+				cout << "  O";
+			else if (blocks[3 * i + j] == -2)
+				cout << "  X";
+			else
+				cout << "  " << blocks[3 * i + j];
+			if (j != 2)
 			{
-				flag = FALSE;
-				break;
+				cout << "  |";
 			}
 		}
-		if (!flag)
+		cout << endl;
+		if (i < 2)
+		{
+			cout << "_____|_____|_____" << endl;
+			cout << "     |     |     " << endl;
+		}
+	}
+	cout << "     |     |     " << endl << endl;
+}
+void ground::update_ground(int numblock, int who, player* pl)
+{
+	if (blocks[numblock] == -1 || blocks[numblock] == -2)
+	{
+		cout << "this block is full, please select another one" << endl;
+		pl->write_move();
+	}
+	else
+	{
+		blocks[numblock] = who;
+	}
+}
+void ground::judge()
+{
+	for (int i = 0; i < winblocks.size(); i++)
+	{
+		if (blocks[winblocks[i][0]] == blocks[winblocks[i][1]] && blocks[winblocks[i][1]] == blocks[winblocks[i][2]])
+		{
+			if (blocks[winblocks[i][0]] == -2)
+			{
+				cout << " congratulations, you wiiiiiiiin :))" << endl;
+				exit(0);
+			}
+			else
+			{
+				cout << "oh no, you lose:((" << endl;
+				exit(0);
+			}
+		}
+	}
+	bool flag = TRUE;
+	for (int i = 0; i < 9; i++)
+	{
+		if (blocks[i] != -1 && blocks[i] != -2)
+		{
+			flag = FALSE;
 			break;
+		}
 	}
 	if (flag)
 	{
 		cout << "Draw:)!" << endl;
 		exit(0);
 	}
+}
+
+//function of player class 
+player::player(io_service& io_service, tcp::acceptor& acc)
+	:sock(io_service)
+{
+	acc.accept(sock);
+}
+void player::playgame()
+{
+	while (1)
+	{
+		read_move();
+		write_move();
+	}
+}
+void player::write_move()
+{
+	string msg;
+	getline(cin, msg);
+	msg += "\n";
+	int num = atoi(msg.c_str());
+	if (num < 1 || num>9)
+	{
+		cout << "This block is not exist" << endl;
+		write_move();
+	}
+	gr.update_ground(num - 1, -2, this);
+	gr.show_ground();
+	write(sock, boost::asio::buffer(msg));
+	gr.judge();
+}
+void player::read_move()
+{
+	boost::asio::streambuf buff;
+	read_until(sock, buff, "\n");
+	string s = buffer_cast<const char*>(buff.data());
+	int num = atoi(s.c_str());
+	gr.update_ground(num - 1, -1, this);
+	gr.show_ground();
+	gr.judge();
+}
+
+//main function
+int main()
+{
+	gr.show_ground();
+	io_service io;
+	tcp::acceptor acc(io, tcp::endpoint(tcp::v4(), 1234));
+	player pl(io, acc);
+	pl.playgame();
 }
