02cb04a88c5d166f5df69c188b8141a4eef19f08
shirinbehnami <hhagh2002@gmail.com>
2021-04-01 00:51:30 +0430

    final server:))))))

--- server.cpp
+++ server.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <boost/asio.hpp>
-#include <thread>
 #include <vector>
 #include <string>
 
@@ -8,50 +7,36 @@ using namespace std;
 using namespace boost::asio;
 using namespace ip;
 
-char msg[200];
-void sendTo(tcp::socket& sock)
-{
-	while (1) {
-		string s;
-		getline(cin, s);
-		s += msg;
-		write(sock, boost::asio::buffer(s));
-	}
-}
-
-
-void receiveFrom(tcp::socket& sock)
-{
-	while (1) {
-		boost::asio::streambuf buff;
-		read_until(sock, buff, "\n");
-		strcpy_s(msg,buffer_cast<const char*>(buff.data()));
-	}
-}
-
-
 
 int main()
 {
-	//first
 	io_service io;
 	tcp::socket server_sock(io);
 	tcp::acceptor acc(io, tcp::endpoint(tcp::v4(), 1234));
 	acc.accept(server_sock);
-	thread t1(sendTo, ref(server_sock));
-	thread t2(receiveFrom, ref(server_sock));
 
 	tcp::socket server_sock2(io);
 	tcp::acceptor acc2(io, tcp::endpoint(tcp::v4(), 1234));
 	acc.accept(server_sock2);
-	thread t3(sendTo, ref(server_sock2));
-	thread t4(receiveFrom, ref(server_sock2));
-	
+
+	while (1)
+	{
+		string msg;
+		boost::asio::streambuf buff;
+		read_until(server_sock, buff, "\n");
+		msg=buffer_cast<const char*>(buff.data());
+
+		write(server_sock2, boost::asio::buffer(msg));
+
+		msg = "";
+		boost::asio::streambuf buff2;
+		read_until(server_sock2, buff2, "\n");
+		msg=buffer_cast<const char*>(buff2.data());
+
+		write(server_sock, boost::asio::buffer(msg));
+	}
 
 
-	t1.join();
-	t2.join();
-	t3.join();
-	t4.join();
+
 
 }
